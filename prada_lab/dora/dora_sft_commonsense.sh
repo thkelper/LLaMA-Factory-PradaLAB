ca99040f758099cf8bd44f4e74d781b3b3d4b057
thkelper <thkelper@outlook.com>
2024-05-08 17:57:32 +0800

    update

---
+++ prada_lab/dora/dora_sft_commonsense.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+model_path=/root/autodl-tmp/models/llama-8b-instruct
+model_name=L8BI
+ft_name=dora
+# dataset=commonsense_170k
+dataset=math_10k
+lr=9e-4
+ptbs=2
+pebs=1
+gas=16
+epoch=3.0
+project=DoRA_commonsense
+entity=prada-lab
+output_dir=/root/autodl-tmp/train_exps/${ft_name}_${model_name}_${dataset}_epoch${epoch}_lr${lr}_pbs${ptbs}_gas${gas}
+
+CUDA_VISIBLE_DEVICES=0 python src/train_bash.py \
+	    --stage sft \
+	    --do_train \
+		--model_name_or_path ${model_path} \
+		--dataset ${dataset} \
+		--template default \
+		--finetuning_type lora \
+		--use_dora \
+		--lora_target q_proj,v_proj \
+		--output_dir ${output_dir} \
+		--overwrite_cache \
+		--per_device_train_batch_size 2 \
+		--gradient_accumulation_steps 16 \
+		--lr_scheduler_type cosine \
+		--logging_steps 10 \
+		--learning_rate 9e-4 \
+		--num_train_epochs 6 \
+		--warmup_ratio 0.1 \
+		--plot_loss \
+		--fp16 \
+		--report_to wandb \
+		--wandb_project ${project} \
+		--wandb_entity ${entity}
+		# --overwrite_output_dir \
