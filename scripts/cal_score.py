5bf0d07ba79c4e3ad83c777ade8861f214529be6
KelperYoung <thkelper@outlook.com>
2024-05-14 19:37:06 +0800

    update

--- scripts/cal_score.py
+++ scripts/cal_score.py
@@ -13,10 +13,10 @@ log_path = glob.glob(os.path.join(folder, '*.log'))[0]
 lines = [line.strip() for line in open(log_path).readlines()]
 ceval_scores = [float(line.split(' ')[-1]) for line in lines[:-3]]
 
-logger.info(f'ceval score: {sum(ceval_scores)/len(ceval_scores):.4}')
+print(f'ceval score: {sum(ceval_scores)/len(ceval_scores):.4}')
 
 for line in lines[-3:]:
     assert 'Electric Industry' in line, 'Dataset need to be check'
     dataset_name, dataset_score = line.split(':')
-    logger.info(f'{dataset_name} score {dataset_score}')     
+    print(f'{dataset_name} score {dataset_score}')
 
